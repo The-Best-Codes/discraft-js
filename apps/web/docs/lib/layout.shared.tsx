b94f4ac7bc77a868850aaa2e90a08bdeec13da3b
The-Best-Codes <bestcodes.official@gmail.com>
2025-09-10 12:20:11 -0500

    wip

--- apps/web/docs/lib/layout.shared.tsx
+++ apps/web/docs/lib/layout.shared.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -20,11 +20,12 @@ export function baseOptions(): BaseLayoutProps {
           >
             <circle cx={12} cy={12} r={12} fill="currentColor" />
           </svg>
-          My App
+          Discraft
         </>
       ),
     },
     // see https://fumadocs.dev/docs/ui/navigation/links
     links: [],
+    githubUrl: "https://github.com/The-Best-Codes/discraft-js",
   };
 }
